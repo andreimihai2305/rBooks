6f7e12aeea9481a8ba14be43b6666f3b04e8ca09
Andrei Mihai <andreimihai2305is@gmail.com>
2022-07-06 14:47:02 +0300

    starting with the book list page

--- src/components/BooksList/BooksList.jsx
+++ src/components/BooksList/BooksList.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./BooksList.css";
+
+const demoBooks = [
+    {
+        title: "Beyond Good and Evil",
+        subtitle: "Prelude to a Philosophy of the Future",
+        author: "Friedrich Nietzsche",
+        yearPublished: 1886
+    },
+    {
+        title: "12 Rules for Life",
+        subtitle: "An antidote to chaos",
+        author: "Jordan B. Peterson",
+        yearPublished: 2018
+    },
+    {
+        title: "Crime and Punishment",
+        subtitle: null,
+        author: "Fyodor Dostoevsky",
+        yearPublished: 1866
+    }
+];
+
+
 
 function BooksList() {
+    
+    const [books, setBooks] = useState(demoBooks);
+    const listItems = books.map(book => <li key={book.title} className='book-title'>{book.title}</li>)
     return (
-        <h1>Books List</h1>
+        <div className='main-list'>
+            <h1 className='page-title'>Books List:</h1>
+            <ul  className='books-list'>
+                {listItems}
+            </ul>
+        </div>
     );
 }
 
