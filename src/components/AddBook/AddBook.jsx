3f56585d313d030a58ecd04af30656c9dd247c88
Andrei Mihai <andreimihai2305is@gmail.com>
2022-07-07 12:48:21 +0300

    started work on the Add book page

--- src/components/AddBook/AddBook.jsx
+++ src/components/AddBook/AddBook.jsx
@@ -3,7 +3,24 @@ import './AddBook.css';
 
 function AddBook() {
     return (
-        <h1>Add Book</h1>
+        <div className='page'>
+            <h1 className='page-title'>Add a book</h1>
+            <form className="book-form">
+                <div className='input-section'>
+                    <label htmlFor="title">Book Title: </label> <br/>
+                    <input className='add-book-input' type="text" name="title" id="title"/>
+                </div>
+                <div className='input-section'>
+                    <label htmlFor="title">Author: </label> <br/>
+                    <input className='add-book-input' type="text" name="title" id="title"/>
+                </div>
+                <div className='input-section'>
+                    <label htmlFor="title">Year published: </label> <br/>
+                    <input className='add-book-input' type="text" name="title" id="title"/>
+                </div>
+                <button className="submit-button" type='submit'>Add Book</button>
+            </form>
+        </div>
     );
 }
 
