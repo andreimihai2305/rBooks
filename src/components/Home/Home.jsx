5d4d018f4cd2a3f01aa428d564538a7525b0c1b2
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-03 22:11:17 +0300

    adding simple home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
       <section>
           <h1 className='page-title'>Home</h1>
-
+          <h2 className='page-subtitle'>Your reading tracking app</h2>
       </section>
   );
 }
