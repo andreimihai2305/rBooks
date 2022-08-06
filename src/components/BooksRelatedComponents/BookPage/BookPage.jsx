d6dae424d1c8ab478e95362e7de39007d6a8b4a0
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-06 11:43:57 +0300

    added button to navigate back from book preview page

--- src/components/BooksRelatedComponents/BookPage/BookPage.jsx
+++ src/components/BooksRelatedComponents/BookPage/BookPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './BookPage.css'
-import { useParams } from 'react-router-dom';
+import { useParams,  useNavigate } from 'react-router-dom';
 import titleToLink from "../formatTitle";
 
 
@@ -8,15 +8,17 @@ function BookPage(props) {
 
     const { books } = props;
     const params = useParams();
-
+    const navigate = useNavigate(); 
     const book = books.filter(book => titleToLink(book.title) === params.title)[0];
     return (
         <>
         {book && <div className="book-page-content">
-            <h1 className="page-title book-page-title">{book.title}</h1>
-            <p className="book-subtitle book-page-subtitle">{book.subtitle}</p>
-            <p className="book-page-author">by {book.author}, year: {book.yearPublished}</p>
-
+            <div>
+                <h1 className="page-title book-page-title">{book.title}</h1>
+                <p className="book-subtitle book-page-subtitle">{book.subtitle}</p>
+                <p className="book-page-author">by {book.author}, year: {book.yearPublished}</p>
+            </div>
+            <button className="see-book" onClick={() => navigate(-1)}>Go back</button>
 
         </div>}
 
