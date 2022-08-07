b41915b6c31e10219e948916e04df6a222f7e568
Andrei Mihai <andreimihai2305is@gmail.com>
2022-08-07 19:20:24 +0300

    adding books context provider

---
+++ src/contexts/BooksContext.jsx
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect, useState } from "react";
+import demoBooks from "../demoBooks";
+const BooksContext = React.createContext({});
+
+export const useBooks = () => useContext(BooksContext);
+
+export default function BooksProvider({ children }) {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/books-list")
+      .then((res) => res.json())
+      .then((books) => setBooks(books))
+      .catch((error) => {
+        console.error(`Error occured!\n ${error.message}`);
+        setBooks(demoBooks);
+      });
+  }, []);
+  console.log(books);
+  return (
+    <BooksContext.Provider value={{ books }}>{children}</BooksContext.Provider>
+  );
+}
